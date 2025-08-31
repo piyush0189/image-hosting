f33d27460979689965851b1e4dfcb6365fbd753e
piyush0189 <piyushpushkar2004@gmail.com>
2025-08-31 15:31:17 +0530

    Remove models folder to fix deployment

--- models/Image.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const imageSchema = new mongoose.Schema({
-  url: { type: String, required: true },
-  public_id: { type: String, required: true },
-  uploadedAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Image", imageSchema);
